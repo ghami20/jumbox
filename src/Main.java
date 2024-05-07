16469ef5283f5b89c6dd4dc0a798f035c885f004
Gamaliel Quiroz <ghamiboxbox@gmail.com>
2024-05-07 14:26:34 -0300

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "HOLA :D");
+	}
+
+}
