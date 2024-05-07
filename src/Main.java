64d3da30276b6e0b0724f01a3ea620db49a4c940
Gamaliel Quiroz <ghamiboxbox@gmail.com>
2024-05-07 14:32:30 -0300

    Arreglo en main

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JOptionPane.showMessageDialog(null, "HOLA :D");
-		
-		JOptionPane.showMessageDialog(null, "Estoy jugando");
-		int num = 1/0;
+	
 	}
 
 }
