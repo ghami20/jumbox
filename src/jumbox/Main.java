2874c7bc9aec5232d78bb4f6084e1702fb46f602
Gamaliel Quiroz <ghamiboxbox@gmail.com>
2024-05-07 14:46:00 -0300

    cambio en repositorio

--- src/jumbox/Main.java
+++ src/jumbox/Main.java
@@ -1,11 +1,12 @@
+package jumbox;
+
 import javax.swing.JOptionPane;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JOptionPane.showMessageDialog(null, "HOLA :D");
-	
+		JOptionPane.showMessageDialog(null, "Hola");
 	}
 
 }
